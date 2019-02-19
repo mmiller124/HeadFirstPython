556a73761264f065ec25e261ab8d7ceea923a50f
Michael Miller <m.scott.miller9@gmail.com>
2019-02-18 23:21:53 -0500

    still wanting this to work

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HeadFirstPython
 Where I keep all my code from Head First Python book
+
+I am just using this for now to practice with Git and GitBash and GitHub
---
+++ StillOdd.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+odds = range(1,60,2)
+
+right_this_minute = datetime.today().minute
+
+if right_this_minute in odds:
+    print("This minute seems a little odd.")
+else:
+    print("Not an odd minute.")
+
+import time
+
+print(time.strftime("%H %M"))
