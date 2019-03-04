dbcfa46c824414f5bf9d0c08331ecc949dd737c6
Michael <m.scott.miller9@gmail.com>
2019-03-03 21:12:45 -0500

    end of chapter 4 push

---
+++ mystery.py
@@ -0,0 +1,15 @@
+
+
+def double(arg):
+    print('Before: ',arg)
+    arg = arg * 2
+    print('After: ',arg)
+
+
+def change(arg):
+    print('Before: ',arg)
+    arg = arg*1
+    arg.append('More data')
+    print('After: ',arg)
+
+
